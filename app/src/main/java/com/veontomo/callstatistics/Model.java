ca2a1e52019a7b4480379a58173b1b81a506ef71
Mario Rossi <veontomo@gmail.com>
2016-04-26 09:26:36 +0200

    feat: make the model use RxJava

--- app/src/main/java/com/veontomo/callstatistics/Model.java
+++ app/src/main/java/com/veontomo/callstatistics/Model.java
@@ -9,6 +9,11 @@ import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Observable;
+
+import rx.Subscriber;
+
 
 /**
  * Model of MVP architecture
@@ -19,8 +24,50 @@ public class Model {
 
     private ArrayList<Call> mCalls;
 
+    private final Subscriber<List<Call>> mCallsReceiver;
+
     public Model(final Presenter presenter) {
         mPresenter = presenter;
+        mCallsReceiver = new Subscriber<List<Call>>(){
+
+            /**
+             * Notifies the Observer that the {@link Observable} has finished sending push-based notifications.
+             * <p/>
+             * The {@link Observable} will not call this method if it calls {@link #onError}.
+             */
+            @Override
+            public void onCompleted() {
+
+            }
+
+            /**
+             * Notifies the Observer that the {@link Observable} has experienced an error condition.
+             * <p/>
+             * If the {@link Observable} calls this method, it will not thereafter call {@link #onNext} or
+             * {@link #onCompleted}.
+             *
+             * @param e the exception encountered by the Observable
+             */
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            /**
+             * Provides the Observer with a new item to observe.
+             * <p/>
+             * The {@link Observable} may call this method 0 or more times.
+             * <p/>
+             * The {@code Observable} will not call this method again after it calls either {@link #onCompleted} or
+             * {@link #onError}.
+             *
+             * @param calls the item emitted by the Observable
+             */
+            @Override
+            public void onNext(List<Call> calls) {
+
+            }
+        };
     }
 
     /**
